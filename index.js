848896eae7338de0c8383bc5368852e7576bf428
SeungJin051 <seungjin051@gmail.com>
2024-09-10 15:04:44 +0900

    수업 0910

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+	res.send('Hello World!');
+});
 
-app.listen(5000, function() {
-  console.log('Express app port 5000');
-})
+app.listen(5000, function () {
+	console.log('Express app port 5000');
+});
