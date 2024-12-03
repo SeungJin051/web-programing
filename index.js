e925795aae41edee24663658acdbb8c9a3c673dd
SeungJin051 <seungjin051@gmail.com>
2024-12-03 14:18:03 +0900

    1203

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
+const connect = require('./schemas');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
+
+const app = express();
+app.set('port', process.env.PORT || 3002);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+	express: app,
+	watch: true,
+});
+connect();
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
+
+app.use((req, res, next) => {
+	const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+	error.status = 404;
+	next(error);
 });
 
-app.listen(5000, function () {
-	console.log('Express app port 5000');
+app.use((err, req, res, next) => {
+	res.locals.message = err.message;
+	res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+	console.log(app.get('port'), '번 포트에서 대기 중');
 });
